0383bec396727145a26a03dd955c46c470e1ba3a
boldt8 <168874236+boldt8@users.noreply.github.com>
2025-12-08 04:07:38 +0000

    finished road and cities problem

--- P1.cpp
+++ P1.cpp
@@ -5,6 +5,40 @@
 #define endl '\n'
 using namespace std;
 
+
+struct DSU {
+    vector<int> v;
+    int numisl;
+
+    DSU(int n) {
+        v = vector<int>(n,-1);
+        numisl = n;
+    }
+
+    int get(int x) {
+        return v[x] < 0 ? x : v[x] = get(v[x]);
+    }
+
+    bool same_set(int a, int b) {
+        return get(a) == get(b);
+    }
+
+    int size(int x) {
+        return -v[get(x)];
+    }
+
+    bool unite (int x, int y){
+        x = get(x), y = get(y);
+        if (x == y) return false;
+        if (v[x] > v[y]) swap(x,y);
+        v[x] += v[y];
+        v[y] = x;
+        numisl--;
+        return true;
+    }
+
+};
+
 signed main() {
     if(!DEBUG){
         ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
@@ -13,4 +47,25 @@ signed main() {
         (void)freopen("test.in", "r", stdin);
         (void)freopen("test.out", "w", stdout); 
     }
+
+    int n, m;
+    cin>>n>>m;
+    DSU thing = DSU(n);
+    int maxsize = 1;
+    
+
+    for(int i = 0; i < m; i++){
+        int a, b;  
+        cin>>a>>b;
+        a--,b--;
+        if(thing.unite(a,b)==false){
+            cout<<thing.numisl<<" "<<maxsize<<endl;
+        }
+        else{
+            maxsize= max(maxsize, thing.size(a));
+            cout<<thing.numisl<<" "<<maxsize<<endl;
+        }
+
+    }
 }
+
